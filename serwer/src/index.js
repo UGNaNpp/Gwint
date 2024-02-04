39aa4c4d922f41469bdb09e8bb7ccdd78db99aa0
Michał Kopczewski <kopczewski.michal101@gmail.com>
2024-02-04 02:20:35 +0100

    Backend rejestracji w serwisie, bez validacji danych

--- serwer/src/index.js
+++ serwer/src/index.js
@@ -3,10 +3,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 const Yup = require("yup");
-const { MongoClient, ObjectId } = require("mongodb"); 
+const { MongoClient, ObjectId } = require("mongodb");
 const passwords = require("./../passwords.json");
-const register = require("./login").register; 
-
+const register = require("./login").register;
 
 app.use(express.text({ type: "text/*" }));
 app.use(bodyParser.json());
@@ -14,21 +13,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = 8080;
 
-
 app.post("/register", (req, res) => {
   const registerData = req.body;
   register(registerData)
-  .then((result) => res.send(result))
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error Occured");
-  })
-})
-
-
-
-
-
+    .then((result) => res.send(result))
+    .catch((err) => {
+      switch (err.message) {
+        case "Email exists.":
+          res.status(409).send(err.message);
+          break;
+        default: {
+          console.error(err);
+          res.status(500).send("Error Occured.");
+          break;
+        }
+      }
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}/`);
--- serwer/src/login.js
+++ serwer/src/login.js
@@ -1,32 +1,27 @@
 const { MongoClient, ObjectId } = require("mongodb");
 const passwords = require("../passwords.json");
 const bcrypt = require("bcrypt");
+const { query } = require("express");
 
 const connectionString = `mongodb+srv://${passwords.mongo.username}:${passwords.mongo.password}@cluster0.0xx1rb1.mongodb.net/?retryWrites=true&w=majority`;
 
 // TODO validacja
-function register(registerData) {
-  try {
-    return new Promise((resolve, reject) => {
-      const client = new MongoClient(connectionString);
-      hashPassword(registerData.password).then(
-        (passwordHash) => {
-          delete registerData.password;
-          const collection = client.db("Gwint").collection("Users");
-          const result = collection
-            .insertOne({
-              ...registerData,
-              points: 0,
-              passwordHash: passwordHash,
-            })
-            .then(() => {
-              resolve(result);
-            });
-        }
-      );
+async function register(registerData) {
+  if ( await isEmaiInDb(registerData.email)) {
+    throw new Error("Email exists.");
+  } else {
+    const client = new MongoClient(connectionString);
+    const passwordHash = await hashPassword(registerData.password);
+    const collection = client.db("Gwint").collection("Users");
+    delete registerData.password;
+    const queryRes = await collection.insertOne({
+      ...registerData,
+      points: 0,
+      passwordHash: passwordHash,
     });
-  } catch (error) {
-    return error;
+    await client.close();
+    const userId = queryRes.insertedId;
+    return userId;
   }
 }
 
@@ -41,6 +36,17 @@ function hashPassword(plainPassword) {
       }
     });
   });
+}
+
+async function isEmaiInDb(email) {
+  const client = new MongoClient(connectionString);
+  const collection = client.db("Gwint").collection("Users");
+  const queryRes = await collection.findOne({
+    email: email,
+  });
+  await client.close();
+  if (queryRes === null) return false;
+  else return true;
 }
 
 module.exports = { register };
