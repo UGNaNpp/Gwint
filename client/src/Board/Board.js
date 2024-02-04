a1a1105d7ae42f3caf4d3cc2c068ef5a88f330cf
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-02-04 12:31:35 +0100

    wyswietlanie kart na planszy, dodawanie ich na plansze po kliknieciu

--- client/src/Board/Board.js
+++ client/src/Board/Board.js
@@ -4,6 +4,11 @@ import './Board.css';
 
 const Board = () => {
   const [playerCards, setPlayerCards] = useState([]);
+  const [playerCardsOnBoard, setPlayerCardsOnBoard] = useState({
+    melee: [],
+    ranged: [],
+    ballista: [],
+  });
 
   useEffect(() => {
     let cards = [...allCards];
@@ -11,19 +16,51 @@ const Board = () => {
     setPlayerCards(cards.slice(0, 10));
   }, []);
 
+  const handleCardClick = (card) => {
+    setPlayerCards(playerCards.filter(c => c !== card));
+    setPlayerCardsOnBoard(prevState => ({
+      ...prevState,
+      [card.cardClass]: [...prevState[card.cardClass], card]
+    }));
+  };
+
   return (
     <div className="game">
       <div className="board">
         <div className="row opponent-row ballista"></div>
         <div className="row opponent-row ranged"></div>
         <div className="row opponent-row melee"></div>
-        <div className="row player-row melee"></div>
-        <div className="row player-row ranged"></div>
-        <div className="row player-row ballista"></div>
+        <div className="row player-row melee">
+          {playerCardsOnBoard.melee.map((card, index) => (
+            <div key={index} className="card">
+              <div>{card.name}</div>
+              <div>{card.cardClass}</div>
+              <div>{card.power}</div>
+            </div>
+          ))}
+        </div>
+        <div className="row player-row ranged">
+          {playerCardsOnBoard.ranged.map((card, index) => (
+            <div key={index} className="card">
+              <div>{card.name}</div>
+              <div>{card.cardClass}</div>
+              <div>{card.power}</div>
+            </div>
+          ))}
+        </div>
+        <div className="row player-row ballista">
+          {playerCardsOnBoard.ballista.map((card, index) => (
+            <div key={index} className="card">
+              <div>{card.name}</div>
+              <div>{card.cardClass}</div>
+              <div>{card.power}</div>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="cards">
         {playerCards.map((card, index) => (
-          <div key={index} className="card">
+          <div key={index} className="card" onClick={() => handleCardClick(card)}>
             <div>{card.name}</div>
             <div>{card.cardClass}</div>
             <div>{card.power}</div>
