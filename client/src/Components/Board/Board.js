bf9185110b6e17b078b38df6848f62864f61e4b9
Michał Kopczewski <kopczewski.michal101@gmail.com>
2024-02-07 16:09:53 +0100

    Dodano profilaktyczne czyszczenie planszy przy starcie gry

--- client/src/Components/Board/Board.js
+++ client/src/Components/Board/Board.js
@@ -1,19 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import CardDisplay from './CardDisplay.js';
-import './Board.css';
-import axios from 'axios';
-import Card from "./../Cards/Card.js"
+import React, { useState, useEffect } from "react";
+import CardDisplay from "./CardDisplay.js";
+import "./Board.css";
+import axios from "axios";
+import Card from "./../Cards/Card.js";
 const config = require("./../../Resources/config.json");
 const developUserId = "f8d13d62-0124-4c07-901d-507e6ba45b59"; //TODO zrób to z ciasteczka
-// ! Jeśli ja kiedyś zostawię tyle komentarzy w kodzie błagam przyjdzcie i mnie ubijcie.
-// ! Jak na moje oko ten kod jest świetnie samokomentujący więc to nie jest potrzebne.
 
 const Board = () => {
-
   // karty przydzielone graczowi (karty w ręce)
   const [playerCards, setPlayerCards] = useState([]);
   const [opponentCards, setOpponentCards] = useState([]);
-
 
   // karty na planszy gracza
   const [playerCardsOnBoard, setPlayerCardsOnBoard] = useState({
@@ -21,7 +17,6 @@ const Board = () => {
     ranged: [],
     ballista: [],
   });
-
 
   // karty na planszy przeciwnika
   const [opponentCardsOnBoard, setOpponentCardsOnBoard] = useState({
@@ -39,28 +34,46 @@ const Board = () => {
   const [opponentPassed, setOpponentPassed] = useState(false);
   const [playerCrystals, setPlayerCrystals] = useState(2);
   const [opponentCrystals, setOpponentCrystals] = useState(2);
-  
-    function startGame() {
-      axios
-      .post(`${config.serverURL}/new-game`, {creatorId: developUserId})
+
+  function startGame() {
+    const resetBoard = () => {
+      setPlayerCardsOnBoard({
+        melee: [],
+        ranged: [],
+        ballista: [],
+      });
+      setOpponentCardsOnBoard({
+        melee: [],
+        ranged: [],
+        ballista: [],
+      });
+    };
+    resetBoard();
+    axios
+      .post(`${config.serverURL}/new-game`, { creatorId: developUserId })
       .then((response) => {
-        console.log(response)
-      setPlayerCards(response.data.playerDeck.map((card) => Card.createFromJSObject(card)))
-      setOpponentCards(response.data.opponentDeck.map((card) => Card.createFromJSObject(card))) //! tylko developersko
+        console.log(response);
+        setPlayerCards(
+          response.data.playerDeck.map((card) => Card.createFromJSObject(card))
+        );
+        setOpponentCards(
+          response.data.opponentDeck.map((card) =>
+            Card.createFromJSObject(card)
+          )
+        ); //! tylko developersko
       })
-      .catch(err => console.error(err))
-    }
-  
-    useEffect(() => {
-      startGame()
-    }, []);
+      .catch((err) => console.error(err));
+  }
+
+  useEffect(() => {
+    startGame();
+  }, []);
 
   useEffect(() => {
     console.log(playerPassed);
   }, [playerPassed]);
 
   const [botMoves, setBotMoves] = useState(0);
-
 
   // czy gracz ma teraz ruch
   const [isPlayerTurn, setIsPlayerTurn] = useState(true);
@@ -72,15 +85,16 @@ const Board = () => {
     }
 
     // po kliknieciu
-    setPlayerCards(playerCards.filter(c => c !== card)); //usuwa karte z reki gracza
-    setPlayerCardsOnBoard(prevState => { //dodaje na plansze
+    setPlayerCards(playerCards.filter((c) => c !== card)); //usuwa karte z reki gracza
+    setPlayerCardsOnBoard((prevState) => {
+      //dodaje na plansze
       const updatedState = {
         ...prevState,
-        [card.cardClass]: [...prevState[card.cardClass], card]
+        [card.cardClass]: [...prevState[card.cardClass], card],
       };
       return updatedState;
     });
-    setScore(prevScore => prevScore + card.power); //dodaje punkty. (później punkty powinny być liczone dla każdego rzędu z osobna)
+    setScore((prevScore) => prevScore + card.power); //dodaje punkty. (później punkty powinny być liczone dla każdego rzędu z osobna)
 
     if (!opponentPassed) {
       setIsPlayerTurn(false);
@@ -91,9 +105,9 @@ const Board = () => {
   //jesli obydwoje gracze spasowali, koniec rundy i reset statystyk
   if (playerPassed && opponentPassed) {
     if (score > enemyScore) {
-      setOpponentCrystals(prevCrystals => prevCrystals - 1);
+      setOpponentCrystals((prevCrystals) => prevCrystals - 1);
     } else if (score < enemyScore) {
-      setPlayerCrystals(prevCrystals => prevCrystals - 1);
+      setPlayerCrystals((prevCrystals) => prevCrystals - 1);
     }
 
     setPlayerPassed(false);
@@ -101,10 +115,10 @@ const Board = () => {
     setScore(0);
     setEnemyScore(0);
     setBotMoves(0);
-
   }
 
-  const handlePassClick = () => { //pasujemy
+  const handlePassClick = () => {
+    //pasujemy
     if (isPlayerTurn && !playerPassed) {
       setPlayerPassed(true);
       setIsPlayerTurn(false);
@@ -121,31 +135,34 @@ const Board = () => {
   const botMove = (playerHasPassed) => {
     setTimeout(() => {
       if (opponentCards.length > 0 && !opponentPassed) {
-        const passChance = botMoves * 0.00; // szansa na spasowanie bota rośnie z każdym ruchem
-        const e = Math.random()
-        console.log(e, "musi byc mniejsze od ", passChance)
+        const passChance = botMoves * 0.0; // szansa na spasowanie bota rośnie z każdym ruchem
+        const e = Math.random();
+        console.log(e, "musi byc mniejsze od ", passChance);
         if (e < passChance) {
-          console.log('chlop spasował')
+          console.log("chlop spasował");
           setOpponentPassed(true);
           setIsPlayerTurn(true);
         } else {
           const botCardIndex = Math.floor(Math.random() * opponentCards.length);
           const botCard = opponentCards[botCardIndex];
-          setBotMoves(prevMoves => prevMoves + 1);
-  
-          setOpponentCards(opponentCards.filter((c, index) => index !== botCardIndex));
-          setOpponentCardsOnBoard(prevState => {
+          setBotMoves((prevMoves) => prevMoves + 1);
+
+          setOpponentCards(
+            opponentCards.filter((c, index) => index !== botCardIndex)
+          );
+          setOpponentCardsOnBoard((prevState) => {
             const updatedState = {
               ...prevState,
-              [botCard.cardClass]: [...prevState[botCard.cardClass], botCard]
+              [botCard.cardClass]: [...prevState[botCard.cardClass], botCard],
             };
             return updatedState;
           });
-  
-          setEnemyScore(prevScore => prevScore + botCard.power);
-          console.log("czy ja spasowalem?", playerHasPassed)
-  
-          if (!playerHasPassed) { // gracz ma ruch o ile wczesniej nie spasował
+
+          setEnemyScore((prevScore) => prevScore + botCard.power);
+          console.log("czy ja spasowalem?", playerHasPassed);
+
+          if (!playerHasPassed) {
+            // gracz ma ruch o ile wczesniej nie spasował
             setIsPlayerTurn(true);
           } else {
             // Jeśli gracz spasował, bot wykonuje kolejny ruch
@@ -173,7 +190,6 @@ const Board = () => {
       </div>
 
       <div className="opponent-cards">
-
         {opponentCards.map((card, index) => (
           <CardDisplay key={index} {...card} />
         ))}
@@ -214,11 +230,15 @@ const Board = () => {
 
       <div className="cards">
         {playerCards.map((card, index) => (
-          <CardDisplay key={index} {...card} onClick={() => handleCardClick(card)} />
+          <CardDisplay
+            key={index}
+            {...card}
+            onClick={() => handleCardClick(card)}
+          />
         ))}
       </div>
     </div>
   );
-}
+};
 
 export default Board;
