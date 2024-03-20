67b3b578bf7a12d79b2cafa9116627a2f72d7783
I-am-not-a-weeb <kubutek04@gmail.com>
2024-03-20 04:23:52 +0100

    routes w app.js

--- new_server/app.js
+++ new_server/app.js
@@ -5,7 +5,21 @@ const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
+    
 });
+
+
+app.patch('/mm')
+app.get('/session/:sessionID')
+app.patch('/session/:sessionID/move')
+app.patch('/session/:sessionID/round')
+
+app.get('/player/:playerID')
+app.get('/player/:playerID/history')
+app.get('/player/:playerID/info') 
+app.patch('/player/:playerID/edit')
+
+app.get('/ranking')
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
