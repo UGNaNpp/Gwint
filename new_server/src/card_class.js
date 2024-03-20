fc4d198890c264fc48cb13d998d70ec424ab2b7b
I-am-not-a-weeb <kubutek04@gmail.com>
2024-03-20 01:09:14 +0100

    zara push

--- new_server/src/card_class.js
+++ new_server/src/card_class.js
@@ -1,5 +1,18 @@
+const deck = {
+    "No_deck": 0,
+    "Monsters":1,
+    "Scoiatel":2,
+    "Nilfgaard":3,
+    "Skellige":4,
+    "Northern_Realms":5,
+}
+
 class Card
 {
+    #name;
+    #picture;
+    #ability = NULL;
+
     constructor(name,picture,ability=NULL)
     {
         this.name = name;
@@ -10,16 +23,21 @@ class Card
 
 class Normal_Card extends Card
 {
+    #strength = 0;
+    #type = NULL;
+    #deck;
+
     constructor(name,picture,ability=NULL,strength,type=NULL,deck)
     {
         super(name,picture,ability);
         this.strength = strength;
-        this.type = 
+        this.type = type;
         this.deck = deck;
     }
 }
 
 class Commander extends Card
 {
+    #deck;
 
 }
