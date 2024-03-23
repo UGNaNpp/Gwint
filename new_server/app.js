7076b6dcdd06170f0066622431c40f88d9cad08d
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-03-23 20:31:24 +0100

    .

--- new_server/app.js
+++ new_server/app.js
@@ -1,13 +1,35 @@
 const express = require('express');
-
 const app = express();
 const port = 3000;
+const uri = 'mongodb://localhost:27003/';
+const MongoClient = require('mongodb').MongoClient;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
     
 });
 
+
+app.post('/api/addData', async (req, res) => {
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        const database = client.db('gwint');
+        const collection = database.collection('users');
+        const newData = req.body;
+        await collection.insertOne(newData);
+
+        res.status(201).send('Dodano użytkownika');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    } finally {
+        await client.close();
+    }
+});
 
 app.patch('/mm')
 app.get('/session/:sessionID')
